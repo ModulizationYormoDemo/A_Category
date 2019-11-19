3f3095c5ac51b36498f4be3a209d0db051ccf31b
Yormo <>
2019-11-19 13:47:24 +0800

    a

---
+++ A_Category/A_Category/CTMediator+A.m
@@ -0,0 +1,20 @@
+//
+//  CTMediator+A.m
+//  A_Category
+//
+//  Created by 含包阁 on 2019/11/18.
+//  Copyright © 2019 含包阁. All rights reserved.
+//
+
+#import "CTMediator+A.h"
+#import "AViewControl"
+
+@implementation CTMediator (A)
+- (UIViewController *)A_aViewController
+{
+    /*
+        AViewController *viewController = [[AViewController alloc] init];
+     */
+    return [self performTarget:@"A" action:@"viewController" params:nil shouldCacheTarget:NO];
+}
+@end
